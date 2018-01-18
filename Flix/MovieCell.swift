93a97445587856d8849b82bd6a4645ae8ceecc41
Rohan <@purdue.edu>
2018-01-17 20:36:58 -0500

    Added new features

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Rohan Gupta on 1/14/18.
+//  Copyright © 2018 Rohan Gupta. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
