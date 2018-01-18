6fff8b988b1fff1b60b83437244a006c3d9573b3
Rohan <@purdue.edu>
2018-01-17 21:22:29 -0500

    Added changes

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Rohan Gupta on 1/17/18.
+//  Copyright © 2018 Rohan Gupta. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
